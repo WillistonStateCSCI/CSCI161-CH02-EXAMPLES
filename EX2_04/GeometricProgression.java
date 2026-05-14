17db31f9344f779602f27b766b0859a27d4743f9
Amanda Skinner <Amanda.Skinner.1@willistonstate.edu>
2026-05-14 18:10:35 +0000

    added GeometricProgression.java

---
+++ EX2_04/GeometricProgression.java
@@ -0,0 +1,21 @@
+//Code Fragment 2.4 from textbook
+public class GeometricProgression {
+    protected long base;
+
+    /* Constructs progression 1, 2, 4, 8, 16, ... */
+    public GeometricProgression(){this(2,1);} //Start at 1 with base of 2
+
+    /* Constructs progression 1, b, b^2, b^3, b^4, ... for base b. */
+    public GeometricProgression(long b){this(b, 1);} //Start at 1
+
+    /* Constructs geometric progression with arbitrary base and start. */
+    public GeometricProgression(long b, long start){
+        super(start);
+        base=b;
+    }
+
+    /* Multiplies the current value by the geometric base. */
+    protected void advance(){
+        current *= base; //multiply current by the geometric base
+    }
+}
