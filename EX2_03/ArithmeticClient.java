9a8d3b022f812570d148a7a9198a1c4b73870ff2
Amanda Skinner <Amanda.Skinner.1@willistonstate.edu>
2026-05-14 16:09:32 +0000

    lunch break

---
+++ EX2_03/ArithmeticClient.java
@@ -0,0 +1,14 @@
+//This is an extra example created to demonstrate the Progression Class
+
+public class ArithmeticClient {
+    public static void main(String[] args) {
+        System.out.println("Creating a progression starting at 0:");
+        ArithmeticProgression prog = new ArithmeticProgression(); //Creates a progression starting at 0.
+        System.out.println("Printing the first 10 terms of the progression:");
+        prog.printProgression(10);
+
+        System.out.println();
+
+        //Need to add demos of other methods
+    }
+}
