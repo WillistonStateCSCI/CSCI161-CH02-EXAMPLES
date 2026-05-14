1c5a27d277519ddec2bdc516ef334eea2e4cc67e
Amanda Skinner <Amanda.Skinner.1@willistonstate.edu>
2026-05-14 17:57:42 +0000

    added ArithmeticClient

--- EX2_03/ArithmeticClient.java
+++ EX2_03/ArithmeticClient.java
@@ -9,8 +9,16 @@ public class ArithmeticClient {
 
         System.out.println();
 
-        //Need to add demos of other methods
+        System.out.println("Creating a new progression, starting at 0 with stepsize 5");
         ArithmeticProgression prog2 = new ArithmeticProgression(5); //Creates a progression with stepsize 5;
+        System.out.println("Printing the first 20 terms of the progression:");
+        prog2.printProgression(20);
         
+        System.out.println();
+
+        System.out.println("Creating a new progression, starting at 3 with stepsize 2");
+        ArithmeticProgression prog3 = new ArithmeticProgression(2, 3); //Creates a progression with start 3 and stepsize 2
+        System.out.println("Printing the first 5 terms of the progression:");
+        prog3.printProgression(5);
     }
 }
